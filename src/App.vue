66e0f08794e01415e80ed07a51a075a073cb79f7
Luciano <lucco6823@gmail.com>
2021-09-21 17:38:37 +0200

    Scheletro

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <main>
+      
+    </main>
+    <Footer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Footer,
   }
 }
 </script>
 
-<style lang="scss">
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+      <a href="/">
+        <img src="@/assets/img/dc-logo.png" alt="Logo DC">
+      </a>
+
+      <nav>
+          <ul>
+              <li><a href="#">CHARACTERS</a></li>
+              <li><a href="#">COMICS</a></li>
+              <li><a href="#">MOVIES</a></li>
+              <li><a href="#">TV</a></li>
+              <li><a href="#">GAMES</a></li>
+              <li><a href="#">COLLECTIBLES</a></li>
+              <li><a href="#">VIDEOS</a></li>
+              <li><a href="#">FANS</a></li>
+              <li><a href="#">NEWS</a></li>
+              <li><a href="#">SHOP</a></li>
+          </ul>
+      </nav>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style lang="scss" scoped>
+    header{
+        text-align: center;
+
+        ul{
+            list-style: none;
+
+            li{
+                text-decoration: none;
+                display: inline-block;
+
+                &:hover{
+                    color: red;
+                }
+            }
+        }
+    }
+</style>
