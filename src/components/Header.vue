c86562718388798174e140bf82ee9c8cecb3a14b
Luciano <lucco6823@gmail.com>
2021-09-21 18:49:40 +0200

    header completo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,47 +1,124 @@
 <template>
-  <div>
-      <a href="/">
-        <img src="@/assets/img/dc-logo.png" alt="Logo DC">
-      </a>
-
-      <nav>
-          <ul>
-              <li><a href="#">CHARACTERS</a></li>
-              <li><a href="#">COMICS</a></li>
-              <li><a href="#">MOVIES</a></li>
-              <li><a href="#">TV</a></li>
-              <li><a href="#">GAMES</a></li>
-              <li><a href="#">COLLECTIBLES</a></li>
-              <li><a href="#">VIDEOS</a></li>
-              <li><a href="#">FANS</a></li>
-              <li><a href="#">NEWS</a></li>
-              <li><a href="#">SHOP</a></li>
-          </ul>
-      </nav>
-  </div>
+    <header>
+        <div class="header-nav container">
+            <div class="logo">
+                <img src="@/assets/img/dc-logo.png" alt="logo.png">
+            </div>
+            <div class="nav-menu">
+                <ul class="nav-items">
+                    <li v-for="(link, index) in links" :key="index" class="nav-item" :class="(link.current) ? 'active' : null">
+                        <a :href="link.url">{{link.text.toUpperCase()}}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
 </template>
 
 <script>
 export default {
     name: "Header",
+
+    data(){
+        return{
+            links:[
+                {
+                    text: "CHARACTERS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "COMICS",
+                    url: "#",
+                    current: true,
+                },
+                {
+                    text: "MOVIES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "TV",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "GAMES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "COLLECTIBLES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "VIDEOS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "FANS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "NEWS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "SHOP",
+                    url: "#",
+                    current: false,
+                },
+            ]
+        }
+    }
 }
 </script>
 
-<style lang="scss" scoped>
-    header{
-        text-align: center;
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "@/style/generals";
 
-        ul{
-            list-style: none;
-
-            li{
-                text-decoration: none;
-                display: inline-block;
-
-                &:hover{
-                    color: red;
-                }
-            }
-        }
+.header-nav{
+    height: 100px;
+    @include center(vertical); 
+    justify-content: space-between;
+}
+.logo {
+    width: 70px;
+    padding: 20px 0px;
+    img{
+        width: 100%;
     }
+}
+.nav-items{
+    list-style: none; 
+    display: flex; 
+    font-size: 13px;
+    line-height: 95px;
+}
+.nav-item{
+    padding: 0px 14px;
+    color: black;
+    cursor: pointer;
+    a{
+        text-decoration: none;
+        color: inherit;
+        font-weight: 600;
+        &:hover{
+        color: $primary;
+    }
+}
+    &:hover{
+        color: $primary;
+        border-bottom: 5px solid $primary;
+    }
+    &.active{
+        color: $primary;
+        border-bottom: 5px solid $primary;
+    }
+}
 </style>
